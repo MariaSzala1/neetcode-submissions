32c25165d4afd71dd3eb37d716f47fb2ed4ef97c
Maria <maria.szala@student.uva.nl>
2026-04-28 14:10:12 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable = 3.4
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "Mary"
+print(type(variable))
+
+variable = [3, 5, 8]
+print(type(variable))
